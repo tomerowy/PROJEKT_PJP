b2d4ce4fbb98fec4b312f7a5ffff35f8d2c629a0
Tomasz Szczepuchowski <tomerrar@gmail.com>
2015-12-10 13:16:23 +0100

    tydzien IV

--- MAZESCAPE/main.cpp
+++ MAZESCAPE/main.cpp
@@ -3,9 +3,18 @@
 #include <allegro5\allegro_image.h>
 #include <allegro5\allegro_native_dialog.h>
 #include <allegro5\allegro_ttf.h>
+#include "player.h"
 
 
-const float FPS = 60;
+
+bool keys[] = { false, false, false, false, false,false, false, false, false};
+enum KEYS{UP, DOWN, LEFT, RIGHT, SPACE, ESCAPE, N, H, L};
+enum STATE{MENU,HOWTO,GAME,GAMEOVER, NEXTLEVEL, LEVEL2};
+
+void InitPlayer(Player &player);
+void DrawPlayer(Player &player);
+
+
 
 int main(void)
 {
@@ -20,11 +29,6 @@ int main(void)
 	}
 
 
-	timer = al_create_timer(1.0 / FPS);	
-	if (!timer) {
-
-		return -1;
-	}
 
 	display = al_create_display(640, 480);
 	if (!display)
@@ -35,57 +39,125 @@ int main(void)
 
 
 	al_set_window_title(display, "- - - - MAZESCAPE - - - - ");
+	timer = al_create_timer(1.0 / 60);
+	if (!timer) {
 
+		return -1;
+	}
+	char *nowagra[] = { "NEW GAME" };
+	char *jakgrac[] = { "HOW TO PLAY" };
+	char *wyjscie[] = { "EXIT" };
+	float gameTime = 0;
+	int frames = 0;
+	int FPS = 0;
+	bool done = false, render = false, draw = true, active = false, goup = false, godown = false, goleft = false, goright = false, isLeft = true, isUp = false, isRight = false, isDown = false;
+
+	int state = MENU;
+	Player player;
+	const int stand = 4;
+	const int maxFrame = 2;
+	int curFrame = 0;
+	int frameCount = 0;
+	int frameDelay = 7;
+	
+	int count = 3000;
+
+	int mapColumns = 32;
+	int mapSize = 768;
+	int tileSize = 20;
+	int tileSize1 = 20;
+
+	
+
+	 int labirynt_1[] = { 
+				  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				  0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+				  0, 1, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+				  0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0,
+				  0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0,
+				  0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0,
+				  0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0,
+				  0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1 ,0,
+				  0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0,
+				  0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+				  0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+				  0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+				  0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0,
+				  0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0,
+				  0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0,
+				  0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+				  0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+				  0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+				  0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+				  0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+				  0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+				  0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+				  0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+				  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+				};
+				
+	
 
 	al_install_keyboard();
 	al_init_image_addon();
 	al_init_font_addon();
 	al_init_ttf_addon();
-
+	
+	
 	ALLEGRO_FONT *font = al_load_ttf_font("FONTS/OratorStd.otf", 20, 2);
 	//GRAFIKA MENU
 	ALLEGRO_BITMAP *menu = al_load_bitmap("GFX/MENU_1.png");
 	ALLEGRO_BITMAP *howtoplay = al_load_bitmap("GFX/HOW_TO_PLAY.png");
 	//GRAFIKA POSTAC
-	ALLEGRO_BITMAP *postac_gora_1 = al_load_bitmap("GFX/POSTAC/POSTAC_GORA_1.png");
-	ALLEGRO_BITMAP *postac_gora_2 = al_load_bitmap("GFX/POSTAC/POSTAC_GORA_2.png");
-	ALLEGRO_BITMAP *postac_gora_3 = al_load_bitmap("GFX/POSTAC/POSTAC_GORA_3.png");
-	ALLEGRO_BITMAP *postac_dol_1 = al_load_bitmap("GFX/POSTAC/POSTAC_DOL_1.png");
-	ALLEGRO_BITMAP *postac_dol_2 = al_load_bitmap("GFX/POSTAC/POSTAC_DOL_2.png");
-	ALLEGRO_BITMAP *postac_dol_3 = al_load_bitmap("GFX/POSTAC/POSTAC_DOL_3.png");
-	ALLEGRO_BITMAP *postac_prawa_1 = al_load_bitmap("GFX/POSTAC/POSTAC_PRAWA_1.png");
-	ALLEGRO_BITMAP *postac_prawa_2 = al_load_bitmap("GFX/POSTAC/POSTAC_PRAWA_2.png");
-	ALLEGRO_BITMAP *postac_prawa_3 = al_load_bitmap("GFX/POSTAC/POSTAC_PRAWA_3.png");
-	ALLEGRO_BITMAP *postac_lewa_1 = al_load_bitmap("GFX/POSTAC/POSTAC_LEWA_1.png");
-	ALLEGRO_BITMAP *postac_lewa_2 = al_load_bitmap("GFX/POSTAC/POSTAC_LEWA_2.png");
-	ALLEGRO_BITMAP *postac_lewa_3 = al_load_bitmap("GFX/POSTAC/POSTAC_LEWA_3.png");
+	ALLEGRO_BITMAP *postacstoi[stand];
+	ALLEGRO_BITMAP *postac[maxFrame];
+	ALLEGRO_BITMAP *postac1[maxFrame];
+	ALLEGRO_BITMAP *postac2[maxFrame];
+	ALLEGRO_BITMAP *postac3[maxFrame];
+	postacstoi[0] = al_load_bitmap("GFX/POSTAC/POSTAC_LEWA_1.png");
+	postacstoi[1] = al_load_bitmap("GFX/POSTAC/POSTAC_GORA_1.png");
+	postacstoi[2] = al_load_bitmap("GFX/POSTAC/POSTAC_DOL_1.png");
+	postacstoi[3] = al_load_bitmap("GFX/POSTAC/POSTAC_PRAWA_1.png");
+	postac[0] = al_load_bitmap("GFX/POSTAC/POSTAC_LEWA_2.png");
+	postac[1] = al_load_bitmap("GFX/POSTAC/POSTAC_LEWA_3.png");
+	postac1[0] = al_load_bitmap("GFX/POSTAC/POSTAC_GORA_2.png");
+	postac1[1] = al_load_bitmap("GFX/POSTAC/POSTAC_GORA_3.png");
+	postac2[0] = al_load_bitmap("GFX/POSTAC/POSTAC_DOL_2.png");
+	postac2[1] = al_load_bitmap("GFX/POSTAC/POSTAC_DOL_3.png");
+	postac3[0] = al_load_bitmap("GFX/POSTAC/POSTAC_PRAWA_2.png");
+	postac3[1] = al_load_bitmap("GFX/POSTAC/POSTAC_PRAWA_3.png");
+	
 	//GRAFIKA LABIRYNT
 	ALLEGRO_BITMAP *podloga = al_load_bitmap("GFX/POZIOMY/POZIOM_1_PODLOGA.png");
-	ALLEGRO_BITMAP *labirynt_cien = al_load_bitmap("GFX/POZIOMY/LABIRYNT_POZIOM_1_CIEN.png");
-	ALLEGRO_BITMAP *labirynt = al_load_bitmap("GFX/POZIOMY/LABIRYNT_POZIOM_1.png");
+	ALLEGRO_BITMAP *labiryntsciana = al_load_bitmap("GFX/labiryntblock.png");
+	ALLEGRO_BITMAP *meta = al_load_bitmap("GFX/meta.png");
 
+	//gameover i next level
+	ALLEGRO_BITMAP *next = al_load_bitmap("GFX/NEXT.png");
+	ALLEGRO_BITMAP *gameover = al_load_bitmap("GFX/GAMEOVER.png");
 
 	ALLEGRO_EVENT_QUEUE *event_queue = al_create_event_queue();
+	
+	player.x = 580;
+	player.y = 40;
+	player.speed = 3;
+
+	int playerh = 18;
+	int playerw = 18;
 	
 	al_register_event_source(event_queue, al_get_display_event_source(display));
 	al_register_event_source(event_queue, al_get_keyboard_event_source());
 	al_register_event_source(event_queue, al_get_timer_event_source(timer));
 	
 	//MENU
-	char *nowagra[] = { "NEW GAME"};
-	char *jakgrac[] = { "HOW TO PLAY" };
-	char *wyjscie[] = { "EXIT" };
+
 	
-	bool done = false, redraw=true;
-	int enter = 0;
-	int choice = 0;
 	al_start_timer(timer);
-	
+	gameTime = al_current_time();
 	while (!done){
 
 		ALLEGRO_EVENT event;
 		al_wait_for_event(event_queue, &event);
-		al_draw_bitmap(menu, 0, 0, 0); //rysuje menu(bez wyboru)
 		al_flip_display();
 
 
@@ -94,128 +166,432 @@ int main(void)
 		{
 			switch (event.keyboard.keycode)
 			{
-			case ALLEGRO_KEY_UP:
-				choice--;
-				break;
-
-			case ALLEGRO_KEY_DOWN:
-				choice++;
-				break;
-
 			case ALLEGRO_KEY_ESCAPE:
-				done = true;
+				keys[ESCAPE]=true;
 				break;
-
-			case ALLEGRO_KEY_ENTER:
-				enter = 1;
+			case ALLEGRO_KEY_LEFT:
+				keys[LEFT] = true;
+				goleft = true;
 				break;
-
+			case ALLEGRO_KEY_RIGHT:
+				keys[RIGHT] = true;
+				goright = true;
+				break;
+			case ALLEGRO_KEY_UP:
+				keys[UP] = true;
+				goup = true;
+				break;
+			case ALLEGRO_KEY_DOWN:
+				keys[DOWN] = true;
+				godown = true;
+				break;
+			case ALLEGRO_KEY_SPACE:
+				keys[SPACE] = true;
+				break;
+			
+			case ALLEGRO_KEY_N:
+				keys[N] = true;
+				break;
+			case ALLEGRO_KEY_H:
+				keys[H] = true;
+				break;
+			case ALLEGRO_KEY_L:
+				keys[L] = true;
+				break;
+			
+		}
+		}
+		else if (event.type == ALLEGRO_EVENT_KEY_UP)
+		{
+			switch (event.keyboard.keycode)
+			{
+			case ALLEGRO_KEY_ESCAPE:
+				keys[ESCAPE] = false;
+				break;
+			case ALLEGRO_KEY_LEFT:
+				keys[LEFT] = false;
+				goleft = false;
+				
+				break;
+			case ALLEGRO_KEY_RIGHT:
+				keys[RIGHT] = false;
+				goright = false;
+				break;
+			case ALLEGRO_KEY_UP:
+				keys[UP] = false;
+				goup = false;
+				break;
+			case ALLEGRO_KEY_DOWN:
+				keys[DOWN] = false;
+				godown = false;
+				break;
+			case ALLEGRO_KEY_SPACE:
+				keys[SPACE] = false;
+				break;
+			case ALLEGRO_KEY_N:
+				keys[N] = false;
+				break;
+			case ALLEGRO_KEY_H:
+				keys[H] = false;
+				break;
+			case ALLEGRO_KEY_L:
+				keys[L] = false;
+				break;
+			
 			}
 
-		}
-		
-	
-
-		//wracanie z exita na new game i zmiana kolorow menu
-		if (choice < 0)
-		{
-			choice = 2;
-		}
-		if (choice > 2)
-		{
-			choice = 0;
 		}
 		else if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
 		{
 			break;
 		}
-
-	
-		for (int i = 0; i < 3; i++)
+		
+		
+		//GAMEUPDATE-----------------------------------------------------
+		
+		else if (event.type == ALLEGRO_EVENT_TIMER)
 		{
-
-			int color1, color2, color3;
-
-			if (i == choice)
+			render = true;
+			
+			
+			
+			
+			frames++;
+			if (al_current_time() - gameTime >= 1)
 			{
-				color1 = 255;
-				color2 = 255;
-				color3 = 255;
+				gameTime = al_current_time();
+				FPS = frames;
+				frames = 0;
 			}
-			else
+			if (state == MENU)
 			{
-				color1 = 64;
-				color2 = 64;
-				color3 = 64;
+				if (keys[ESCAPE])
+					done = true;
 
+				if (keys[H])
+				{
+					state = HOWTO;
+				}
+				if (keys[N])
+				{
+					state = GAME;
+				}
 			}
-
-			al_draw_text(font, al_map_rgb(color1, color2, color3), 275, 190, NULL, *nowagra);
-
-			if (i == choice + 2)
+			else if (state == HOWTO)
 			{
-				color1 = 255;
-				color2 = 255;
-				color3 = 255;
+				if (keys[SPACE])
+					state = MENU;
 			}
-			else
+			else if(state == GAME)
 			{
-				color1 = 64;
-				color2 = 64;
-				color3 = 64;
+				if (++frameCount >= frameDelay)
+				{
+					if (++curFrame >= maxFrame)
+						curFrame = 0;
+					
+					frameCount = 0;
+				}
+				if (keys[ESCAPE]){
+					done = true;
+				}
+				if (keys[UP])
+					player.y -= player.speed;
+				
+				
+
+				if (keys[DOWN])
+					player.y += player.speed;
+				
+				
+				if (keys[LEFT])
+					player.x -= player.speed;
+				
+					
+				if (keys[RIGHT])
+					player.x += player.speed;
+
+				if (player.y < 40)
+					player.y = 40;
+				if (player.y > 420)
+					player.y = 420;
+				if (player.x < 40)
+					player.x = 40;
+				if (player.x > 590)
+					player.x = 590;
+				
+				
+
+				 if (player.x == 40 && player.y == 40){
+					state = NEXTLEVEL;
+				} 
+					
+				
+				
+
+				
+					
+				
 			}
 			
-
-			al_draw_text(font, al_map_rgb(color1, color2, color3), 260, 260, NULL, *jakgrac);
-
-			if (i == choice + 1)
+			else if (state == LEVEL2)
 			{
-				color1 = 255;
-				color2 = 255;
-				color3 = 255;
+				
+				if (++frameCount >= frameDelay)
+				{
+					if (++curFrame >= maxFrame)
+						curFrame = 0;
+
+					frameCount = 0;
+				}
+				if (keys[ESCAPE]){
+					done = true;
+				}
+				if (keys[UP])
+					player.y -= player.speed;
+
+
+
+				if (keys[DOWN])
+					player.y += player.speed;
+
+
+				if (keys[LEFT])
+					player.x -= player.speed;
+
+
+				if (keys[RIGHT])
+					player.x += player.speed;
+
+				if (player.y < 40)
+					player.y = 40;
+				if (player.y > 420)
+					player.y = 420;
+				if (player.x < 40)
+					player.x = 40;
+				if (player.x > 590)
+					player.x = 590;
 			}
-			else
+			else if (state == GAMEOVER)
 			{
-				color1 = 64;
-				color2 = 64;
-				color3 = 64;
+				if (keys[ESCAPE]){
+					done = true;
+				}
+				
 			}
-
-			al_draw_text(font, al_map_rgb(color1, color2, color3), 300, 328, NULL, *wyjscie);
-
-		}
-		redraw = false;
-	
-		
-		
-		/*if (choice == 2 && enter)
-		{
-			enter = 1;
-			al_clear_to_color(al_map_rgb(0, 0, 0));
+			else if (state == NEXTLEVEL)
+			{
+				if (keys[ESCAPE]){
+					done = true;
+				}
+				if (keys[L]){
+					state = LEVEL2;
+				}
+			}
 		}
 	
-		if (choice == 0 && enter)
+		if (render && al_is_event_queue_empty(event_queue))
 		{
-			enter = 1;
-			al_clear_to_color(al_map_rgb(0, 0, 0));
-			al_draw_bitmap(menu, 0, 0, 0);
-			al_draw_bitmap(howtoplay, 0, 0, 0);	
-		}*/
-	
-		if (choice == 1 && enter)
-		{
-			enter = 1;
-			done = true;
-		
-		}
-		
-		if (choice == 2 && enter)
-		{
-			enter = 1;
-			al_clear_to_color(al_map_rgb(0, 0, 0));
 			
+			render = false;
+			switch (state)
+			{
+			case MENU:
+				al_draw_bitmap(menu, 0, 0, 0);
+			
+				break;
+			case HOWTO:
+				al_draw_bitmap(menu, 0, 0, 0);
+				al_draw_bitmap(howtoplay, 0, 0, 0);
+				break;
+			case GAME:
+				al_clear_to_color(al_map_rgb(0, 0, 0));
+				al_draw_bitmap(podloga, 0, 0, 0);
+				
+				for (int i = 0; i < mapSize; i++)
+				{
+					if (labirynt_1[i] == 1)
+					{
+						al_draw_bitmap(labiryntsciana, tileSize*(i%mapColumns), tileSize*(i / mapColumns), 0);
 
+					}
+					if (labirynt_1[i] == 2)
+					{
+						al_draw_bitmap(meta, tileSize*(i%mapColumns), tileSize*(i / mapColumns), 0);
+
+					}
+					
+				}
+				
+
+
+				count--;
+				al_draw_textf(font, al_map_rgb(255, 255, 255), 20, 5, 0, "Time: %i", count);
+				if (count == 0){
+					state = GAMEOVER;
+				}
+				
+				
+
+				if (isLeft)
+				{
+					al_draw_bitmap(postacstoi[0], player.x, player.y, 0);
+				}
+			
+				if(isRight){
+					al_draw_bitmap(postacstoi[3], player.x, player.y, 0);
+				}
+				 if (goleft){
+					
+					al_draw_bitmap(postac[curFrame], player.x, player.y, 0);
+					isLeft = true;
+					isRight = false;
+					isUp = false;
+					isDown = false;
+				}
+				
+				
+				 if (goright){
+					
+					al_draw_bitmap(postac3[curFrame], player.x, player.y, 0);
+					isLeft = false;
+					isRight = true;
+					isUp = false;
+					isDown = false;
+
+				}
+				 if (isUp){
+					 al_draw_bitmap(postacstoi[1], player.x, player.y, 0);
+				 }
+				 if (goup){
+					 al_draw_bitmap(postac1[curFrame], player.x, player.y, 0);
+					 isLeft = false;
+					 isRight = false;
+					 isUp = true;
+					 isDown = false;
+				 }
+				 if (isDown){
+					 al_draw_bitmap(postacstoi[2], player.x, player.y, 0);
+				 }
+				 if (godown)
+				 {
+					 al_draw_bitmap(postac2[curFrame], player.x, player.y, 0);
+					 isLeft = false;
+					 isRight = false;
+					 isUp = false;
+					 isDown = true;
+				 }
+				
+				
+				
+				
+					
+					
+
+				break;
+
+			case LEVEL2:
+				al_clear_to_color(al_map_rgb(0, 0, 0));
+				al_draw_bitmap(podloga, 0, 0, 0);
+
+				
+
+				for (int i = 0; i < mapSize; i++)
+				{
+				if (labirynt_1[i] == 1)
+				{
+					al_draw_bitmap(labiryntsciana, tileSize*(i%mapColumns), tileSize*(i / mapColumns), 0);
+
+				}
+				if (labirynt_1[i] == 2)
+				{
+					al_draw_bitmap(meta, tileSize*(i%mapColumns), tileSize*(i / mapColumns), 0);
+
+				}
+
+			}
+
+
+
+
+			count--;
+			al_draw_textf(font, al_map_rgb(255, 255, 255), 20, 5, 0, "Time: %i", count);
+			if (count == 0){
+				state = GAMEOVER;
+			}
+
+
+
+			if (isLeft)
+			{
+				al_draw_bitmap(postacstoi[0], player.x, player.y, 0);
+			}
+
+			if (isRight){
+				al_draw_bitmap(postacstoi[3], player.x, player.y, 0);
+			}
+			if (goleft){
+
+				al_draw_bitmap(postac[curFrame], player.x, player.y, 0);
+				isLeft = true;
+				isRight = false;
+				isUp = false;
+				isDown = false;
+			}
+
+
+			if (goright){
+
+				al_draw_bitmap(postac3[curFrame], player.x, player.y, 0);
+				isLeft = false;
+				isRight = true;
+				isUp = false;
+				isDown = false;
+
+			}
+			if (isUp){
+				al_draw_bitmap(postacstoi[1], player.x, player.y, 0);
+			}
+			if (goup){
+				al_draw_bitmap(postac1[curFrame], player.x, player.y, 0);
+				isLeft = false;
+				isRight = false;
+				isUp = true;
+				isDown = false;
+			}
+			if (isDown){
+				al_draw_bitmap(postacstoi[2], player.x, player.y, 0);
+			}
+			if (godown)
+			{
+				al_draw_bitmap(postac2[curFrame], player.x, player.y, 0);
+				isLeft = false;
+				isRight = false;
+				isUp = false;
+				isDown = true;
+			}
+			break;
+			
+			case GAMEOVER:
+				al_clear_to_color(al_map_rgb(0, 0, 0));
+				al_draw_bitmap(podloga, 0, 0, 0);
+				al_draw_bitmap(gameover, 0, 0, 0);
+				break;
+			
+			case NEXTLEVEL:
+				al_clear_to_color(al_map_rgb(0, 0, 0));
+				al_draw_bitmap(podloga, 0, 0, 0);
+				al_draw_bitmap(next, 0, 0, 0);
+
+				break;
+			}
 		}
+	
+
+	
+		
+		
+	
+	
 	
 		
 		
@@ -223,17 +599,16 @@ int main(void)
 	
 			
 		al_flip_display();
-		al_clear_to_color(al_map_rgb(0, 0, 0));
+		
 	}
 	
 
 
-
+	
 	al_destroy_timer(timer);
 	al_destroy_display(display); 
 	al_destroy_event_queue(event_queue);
 	return 0;
 
 }
-
 
